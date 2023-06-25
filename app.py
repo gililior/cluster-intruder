eb6f327bc0d09538d70877bda8caecdcf868d005
Gili <gili.lio@mail.huji.ac.il>
2023-06-25 17:12:39 +0300

    multi option checkbox

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ SAMPLE_SIZE = 10
 
 def init(ws_name):
     if 'column_map' not in st.session_state:
-        column_map = {'A': 'username', 'B': 'gold_cluster', 'C': 'answer_iloc',
-                      'D': 'is_correct', 'E': 'intruder_iloc'}
+        column_map = {'A': 'username', 'B': 'gold_cluster', 'C': 'marked',
+                      'D': 'is_correct', 'E': 'confidence', 'F': 'intruder_iloc'}
         for i in range(SAMPLE_SIZE-1):
-            index_letter = i + 5
+            index_letter = i + 6
             letter = string.ascii_uppercase[index_letter]
             column_map[letter] = f"orig_cluster_{i}"
         st.session_state['column_map'] = column_map
@@ -33,24 +33,33 @@ def init(ws_name):
 
 
 def register_results():
-    st.session_state.i += 1
     results = {"gold_cluster": int(st.session_state.current_cluster)}
-    answer = st.session_state.radio
-    correct = st.session_state.radio == st.session_state.df.loc[st.session_state.intruder_index]["title_text"]
+    marked = []
+    for iloc in st.session_state.ilocs:
+        answer_iloc = st.session_state[str(iloc)]
+        if answer_iloc:
+            marked.append(str(iloc))
+    if len(marked) == 0:
+        st.error("You must mark at least one option")
+        return
+    st.session_state.i += 1
+    correct = str(st.session_state.intruder_index) in marked
+    confidence = 1/len(marked)
     for i, iloc in enumerate(st.session_state.indices_cluster):
         results[f"orig_cluster_{i}"] = int(iloc)
-        if st.session_state.df.loc[iloc]["title_text"] == answer:
-            results["answer_iloc"] = int(iloc)
     results["intruder_iloc"] = int(st.session_state.intruder_index)
     results["is_correct"] = correct
-    if correct:
-        results["answer_iloc"] = results["intruder_iloc"]
+    results["confidence"] = confidence
     results["username"] = st.session_state.username
 
     next_row_ind = len(st.session_state.ws.col_values(1)) + 1
     column_map = st.session_state.column_map
+    results["marked"] = ",".join(marked)
+
     for letter in column_map:
+        print(letter)
         if column_map[letter] in results:
+            print(column_map[letter])
             st.session_state.ws.update(letter + str(next_row_ind), results[column_map[letter]])
     st.session_state.update_iteration = True
 
@@ -81,19 +90,36 @@ def main(csv_path):
         from_cluster = random.sample(list(df_cluster.index),
                                      k=min(len(df_cluster), SAMPLE_SIZE-1))
         st.session_state.indices_cluster = from_cluster
-        sentences = df_cluster.loc[from_cluster]["title_text"].tolist()
+        # sentences = df_cluster.loc[from_cluster]["title_text"].tolist()
         assert len(df_cluster.loc[from_cluster]["community"].unique()) == 1
         df_random = st.session_state.df.loc[st.session_state.group_by_cluster[random_cluster]]
         st.session_state.intruder_index = random.choice(df_random.index)
-        sentences.append(df_random.loc[st.session_state.intruder_index]["title_text"])
-        random.shuffle(sentences)
-        st.session_state.sentences = sentences
+        ilocs = from_cluster + [st.session_state.intruder_index]
+        # sentences.append(df_random.loc[st.session_state.intruder_index]["title_text"])
+        random.shuffle(ilocs)
+        st.session_state.ilocs = ilocs
 
     st.progress(value=st.session_state.i+1)
 
-    label = "WHICH SENTENCE DOES NOT BELONG TO THE CLUSTER?"
-    st.radio(label, [label] + st.session_state.sentences, key="radio",
-             on_change=register_results, label_visibility="hidden")
+    st.write("WHICH SENTENCE DOES NOT BELONG TO THE CLUSTER?\n\n"
+             "if you are not sure, you can choose more than one option")
+    # st.session_state.ilocs = {}
+    for iloc in st.session_state.ilocs:
+        sentence = st.session_state.df.iloc[iloc]["title_text"]
+        # st.session_state.ilocs[iloc] = sentence
+        st.checkbox(sentence, value=False, key=iloc)
+
+    st.button(label="submit", on_click=register_results)
+
+
+# def get_iloc(sentence):
+#     if sentence == st.session_state.df.iloc[st.session_state.intruder_index][
+#         "title_text"]:
+#         return st.session_state.intruder_index
+#     else:
+#         for index in st.session_state.indices_cluster:
+#             if sentence == st.session_state.df.iloc[index]["title_text"]:
+#                 return index
 
 
 def hello_page():
